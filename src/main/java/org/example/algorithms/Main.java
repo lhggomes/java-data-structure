7be84433eb197a34966f6b89eafc5df80565bf87
Lucas Gomes <lucas.henrique.s.go@gmail.com>
2024-06-16 14:07:54 -0300

    First Commit

---
+++ src/main/java/org/example/algorithms/Main.java
@@ -0,0 +1,10 @@
+package org.example.algorithms;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int[] intArray = new int[7];
+    }
+}
