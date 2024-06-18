729954c82fef4a76d86272c6ae3ce39716c426f0
Lucas Gomes <lucas.henrique.s.go@gmail.com>
2024-06-17 21:46:36 -0300

    Insert Short

--- src/main/java/org/example/algorithms/Main.java
+++ src/main/java/org/example/algorithms/Main.java
@@ -6,6 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] intArray = {25, -4, 52, 54, -8, 52, -74, -222, 854, 25, 35};
+
+
+        for (int i : intArray) {
+            System.out.println(i);
+        }
 
     }
 
